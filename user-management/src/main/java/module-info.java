4356f95ebf93ad73027952399d1d287dd9858647
AminHaeri <AminHaeri>
2022-07-23 20:32:03 +0430

    test: create new test for user registration test. This is a TDD Red phase.

--- user-management/src/main/java/module-info.java
+++ user-management/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module org.amin.fanoos.usermanagement {
     requires spring.boot.autoconfigure;
     requires spring.boot;
+    requires spring.web;
     exports org.amin.fanoos.usermanagement;
 }
--- user-management/src/test/java/org/amin/fanoos/usermanagement/UserManagementApplicationTests.java
+++ user-management/src/test/java/org/amin/fanoos/usermanagement/UserManagementApplicationTests.java
@@ -1,13 +1,38 @@
 package org.amin.fanoos.usermanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserManagementApplicationTests {
+
+	private final String SIGNUP_BASE_URL = "http://localhost";
+	private final String SIGNUP_REL_PATH = "/api/v1/users";
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	private String getSignupFullUrl() {
+		return SIGNUP_BASE_URL + ":" + port + SIGNUP_REL_PATH;
+	}
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void createNewUser_returnsOk() {
+		ResponseEntity<String> response = restTemplate.getForEntity(getSignupFullUrl(), String.class);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
 }
