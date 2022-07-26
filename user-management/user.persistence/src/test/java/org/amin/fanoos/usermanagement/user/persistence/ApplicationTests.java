1c2ccca245dbc0c9d8b6ccd2ec599845360e6054
AminHaeri <AminHaeri>
2022-07-26 16:18:59 +0430

    refactor: add persistence module as a layer in Hexagonal architecture Now all data persistence based classes (Entity, Repository, DAO, ...) are handled in this layer.

---
+++ user-management/user.persistence/src/test/java/org/amin/fanoos/usermanagement/user/persistence/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amin.fanoos.usermanagement.user.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
