20dda2d6ef6fd2e01e2521fd898cfc85ec8eae57
AminHaeri <AminHaeri>
2022-07-27 15:34:42 +0430

    refactor: change foreign key constraint names in V4 and V5 migrations.

--- user-management/user.persistence/src/main/resources/db/migration/V5__account_many-to-many_role.sql
+++ user-management/user.persistence/src/main/resources/db/migration/V5__account_many-to-many_role.sql
@@ -3,6 +3,6 @@ CREATE TABLE account_role
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     account_id BIGINT NOT NULL,
     role_id    BIGINT NOT NULL,
-    CONSTRAINT FK_Account_TMP FOREIGN KEY (account_id) REFERENCES account (id),
-    CONSTRAINT FK_Role FOREIGN KEY (role_id) REFERENCES role (id)
+    CONSTRAINT FK_AccountRole_Account FOREIGN KEY (account_id) REFERENCES account (id),
+    CONSTRAINT FK_AccountRole_Role FOREIGN KEY (role_id) REFERENCES role (id)
 );
