00748fd6ae610499082336a507b875e2c31387ea
AminHaeri <AminHaeri>
2022-07-31 23:58:38 +0430

    refactor: minor refactors

--- user-management/user.persistence/src/main/java/org/amin/fanoos/usermanagement/user/persistence/UserPersistenceApplication.java
+++ user-management/user.persistence/src/main/java/org/amin/fanoos/usermanagement/user/persistence/UserPersistenceApplication.java
@@ -2,8 +2,10 @@ package org.amin.fanoos.usermanagement.user.persistence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication()
+@EnableJpaRepositories
 public class UserPersistenceApplication {
 
 	public static void main(String[] args) {
