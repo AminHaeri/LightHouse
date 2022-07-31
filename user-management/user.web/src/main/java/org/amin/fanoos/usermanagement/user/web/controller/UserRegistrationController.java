1e0497f0312ce94ec48dbd1a99ba33d49257180e
AminHaeri <AminHaeri>
2022-08-01 00:49:47 +0430

    feat: adding PreAuthorize for registerUser action in UserRegistrationController only for super admin for now.

--- user-management/user.web/src/main/java/org/amin/fanoos/usermanagement/user/web/controller/UserRegistrationController.java
+++ user-management/user.web/src/main/java/org/amin/fanoos/usermanagement/user/web/controller/UserRegistrationController.java
@@ -2,6 +2,7 @@ package org.amin.fanoos.usermanagement.user.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRegistrationController {
 
     @PostMapping
+    @PreAuthorize("hasAuthority(T(org.amin.fanoos.usermanagement.user.application.domain.ERole).ROLE_SUPERADMIN)")
     public ResponseEntity<String> registerUser() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
