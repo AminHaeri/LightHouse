e779a061ab231021e9deb836e2773bdb7f9ecd5b
AminHaeri <AminHaeri>
2022-08-10 17:39:24 +0430

    refactor: move all oauth mock and test to Oauth2 class.

--- user-management/user.web/src/test/java/org/amin/fanoos/usermanagement/user/web/Oauth2Tests.java
+++ user-management/user.web/src/test/java/org/amin/fanoos/usermanagement/user/web/Oauth2Tests.java
@@ -1,27 +1,19 @@
 package org.amin.fanoos.usermanagement.user.web;
 
 import org.amin.fanoos.usermanagement.user.application.domain.ERole;
-import org.amin.fanoos.usermanagement.user.application.domain.User;
-import org.amin.fanoos.usermanagement.user.application.port.out.UserPort;
-import org.amin.fanoos.usermanagement.user.application.port.out.command.UserInfoCommand;
 import org.amin.fanoos.usermanagement.user.web.before.Oauth2;
 import org.amin.fanoos.usermanagement.user.web.before.SuperUserManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.*;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebAppConfiguration
@@ -29,33 +21,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = UserWebApplication.class)
 public class Oauth2Tests {
 
-    private final String OAUTH_REL_PATH = "/oauth/token";
-
     @Autowired
     private Oauth2 oauth2;
 
     @Autowired
     private SuperUserManager superUserManager;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private UserPort userPort;
-
     @Test
     public void createNewJwtToken_withSuperUserCredentials_returnsOkAndTokenResult() throws Exception {
-        User superUser = superUserManager.getSuperUser();
-
-        UserInfoCommand userInfoCommand = new UserInfoCommand(superUser.getAccount().getUserName());
-        given(userPort.getUserByUserName(userInfoCommand)).willReturn(superUser);
-
-        mockMvc.perform(post(OAUTH_REL_PATH)
-                        .header("Authorization", oauth2.getAuthorizationHeaderEncoded())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                        .param("grant_type", "password")
-                        .param("username", superUser.getAccount().getUserName())
-                        .param("password", superUserManager.getSuperUserRawPassword()))
+        oauth2.postUserAuth(superUserManager.getSuperUser(), superUserManager.getSuperUserRawPassword())
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.*", isA(ArrayList.class)))
@@ -69,7 +43,7 @@ public class Oauth2Tests {
     }
 
     @Test
-    public void createNewAuthorizationHeader_withClientIdAndSecretEncoded() throws IOException {
+    public void createNewAuthorizationHeader_withClientIdAndSecretEncoded() throws Exception {
         assertEquals("Authorization header must be correctly encoded base on clientId and clientSecret",
                 "Basic RmFub29zQ2xpZW50OkZhbm9vc1NlY3JldA==",
                 oauth2.getAuthorizationHeaderEncoded());
--- user-management/user.web/src/test/java/org/amin/fanoos/usermanagement/user/web/before/Oauth2.java
+++ user-management/user.web/src/test/java/org/amin/fanoos/usermanagement/user/web/before/Oauth2.java
@@ -1,13 +1,32 @@
 package org.amin.fanoos.usermanagement.user.web.before;
 
+import org.amin.fanoos.usermanagement.user.application.domain.User;
+import org.amin.fanoos.usermanagement.user.application.port.out.UserPort;
+import org.amin.fanoos.usermanagement.user.application.port.out.command.UserInfoCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @Component
 public class Oauth2 {
+
+    private final String OAUTH_REL_PATH = "/oauth/token";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserPort userPort;
 
     @Value("${security.oauth2.client.token-validity}")
     public int tokenValidity;
@@ -22,5 +41,17 @@ public class Oauth2 {
         String raw = clientId + ":" + clientSecret;
         String base64Encoded = Base64.getEncoder().encodeToString(raw.getBytes(StandardCharsets.UTF_8));
         return "Basic " + base64Encoded;
+    }
+
+    public ResultActions postUserAuth(User user, String rawPassword) throws Exception {
+        UserInfoCommand userInfoCommand = new UserInfoCommand(user.getAccount().getUserName());
+        given(userPort.getUserByUserName(userInfoCommand)).willReturn(user);
+
+        return mockMvc.perform(post(OAUTH_REL_PATH)
+                .header("Authorization", this.getAuthorizationHeaderEncoded())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .param("grant_type", "password")
+                .param("username", user.getAccount().getUserName())
+                .param("password", rawPassword));
     }
 }
