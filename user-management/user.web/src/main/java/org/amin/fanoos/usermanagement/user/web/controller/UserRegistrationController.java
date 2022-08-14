c1789946dd55e28cab4ee04a7d453d7b2ce79cb8
AminHaeri <AminHaeri>
2022-08-14 18:29:47 +0430

    feat: implementing the user register command in registerUser method in UserRegistrationController.

--- user-management/user.web/src/main/java/org/amin/fanoos/usermanagement/user/web/controller/UserRegistrationController.java
+++ user-management/user.web/src/main/java/org/amin/fanoos/usermanagement/user/web/controller/UserRegistrationController.java
@@ -1,9 +1,15 @@
 package org.amin.fanoos.usermanagement.user.web.controller;
 
+import org.amin.fanoos.usermanagement.user.application.port.in.command.RegisterUserCommand;
+import org.amin.fanoos.usermanagement.user.application.port.in.usecase.RegisterUserUseCase;
+import org.amin.fanoos.usermanagement.user.web.dto.request.RegisterUserRequestDTO;
+import org.amin.fanoos.usermanagement.user.web.dto.response.RegisterUserResponseDTO;
+import org.amin.fanoos.usermanagement.user.web.mapper.RegisterUserMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/users")
 public class UserRegistrationController {
 
+    private final RegisterUserMapper registerUserMapper;
+    private final RegisterUserUseCase registerUserUseCase;
+
+    public UserRegistrationController(
+            RegisterUserMapper registerUserMapper,
+            RegisterUserUseCase registerUserUseCase) {
+        this.registerUserMapper = registerUserMapper;
+        this.registerUserUseCase = registerUserUseCase;
+    }
+
     @PostMapping
     @PreAuthorize("hasAuthority(T(org.amin.fanoos.usermanagement.user.application.domain.ERole).ROLE_SUPERADMIN)")
-    public ResponseEntity<String> registerUser() {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<RegisterUserResponseDTO> registerUser(
+            @RequestBody RegisterUserRequestDTO registerUserRequestDTO) {
+        RegisterUserCommand cmd = registerUserMapper.toCmd(registerUserRequestDTO);
+        RegisterUserResponseDTO registerUserResponseDTO =
+                registerUserMapper.toResponse(registerUserUseCase.registerUser(cmd));
+
+        return new ResponseEntity<>(registerUserResponseDTO, HttpStatus.CREATED);
     }
 }
